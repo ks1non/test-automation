7886032329775e7fc0cf275d6f14fe8113269b3a
Sergey <s.zhuraev135@gmail.com>
2023-11-05 04:07:01 +0300

    actual push

--- main.py
+++ main.py
@@ -3,3 +3,46 @@
 #
 #
 # summerize(1, 5)
+
+# class Car:
+#     def __init__(self, car_name):
+#         self.car_name = car_name
+#
+#     def create_new_car(self):
+#         print(f'new car created:{self.car_name}')
+#
+#
+# Car('BMW').create_new_car()
+
+
+# a = [1, 2]
+# print(a)
+# a.
+# b = Car('BMW')
+# b.
+#
+# BASEURL = 'www.google.com'
+
+# """1. принимает на вход 1 параметр
+#    2. проверяем валидно ли значение
+#    3. проверяем валидность (только целые число 0 - 10 )"""
+#
+#
+# def validatin(a):
+#     if 0 <= a <= 10 and isinstance(a, int):
+#         print('valid')
+#     elif not isinstance(a, int) and a > 0:
+#         print(f'{a} is not int')
+#     elif a < 0:
+#         print(f"{a} is negative number")
+#     else:
+#         print(f'{a} out of range')
+#
+#
+# validatin(-1.5)
+# validatin(2.5)
+# validatin(0)
+# validatin(1)
+# validatin(9)
+# validatin(10)
+# validatin(11)
--- tests/test_fake.py
+++ tests/test_fake.py
@@ -1,14 +1,16 @@
-# from pytest import mark
-#
-# # @mark.smoke
-# def test_1():
-#     pass
-#
-# @mark.ui
-# def test_2():
-#     pass
-#
-# @mark.ui
+from pytest import mark
+import random
+numbers = [1,2,3]
 # @mark.smoke
-# def test_3():
-#     assert 1 == 1
+def test_1():
+    pass
+
+@mark.ui
+def test_2():
+    pass
+
+@mark.ui
+@mark.smoke
+
+def test_3():
+    assert 1 == 1
